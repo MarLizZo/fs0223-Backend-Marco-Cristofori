c7e2a2e00b1ec7d440064528089c37885259c0a6
MarLizZo <marketto140@libero.it>
2023-06-26 15:20:07 +0200

    first commit

---
+++ m1/s1/g1/src/it/epicode/be/Helloworld.java
@@ -0,0 +1,12 @@
+package it.epicode.be;
+
+public class Helloworld {
+
+		public static void main (String[] args)
+		{
+				System.out.println("This is my first Epicode Java Project!");
+		}
+}
+
+
+
